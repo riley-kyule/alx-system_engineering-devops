ab8ebb51215443f032314cac629dae5fd02f0c2b
riley-kyule <phillipkyule@gmail.com>
2023-05-10 21:28:31 +0300

    Task 0. Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix server error when Apache web server receives GET request via HTTP
 
-exec {'replace':
-  provide => shell,
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
