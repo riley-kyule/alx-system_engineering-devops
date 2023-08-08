0fbf0c560f6242512c0f486e020819011b6a09e1
riley-kyule <phillipkyule@gmail.com>
2023-08-09 00:11:28 +0300

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,12 @@ def number_of_subscribers(subreddit):
     Returns the total number of subs on a given subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    headers = {"User-Agent": 'My Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        data = response.json()
+        subs = data.get("data").get("subscribers")
+        return subs
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
