2802bd5f4e01b0cb538c837a0ebf6b713a03d149
riley-kyule <phillipkyule@gmail.com>
2023-08-09 00:18:02 +0300

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,14 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    headers = {"User-Agent": 'My Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
